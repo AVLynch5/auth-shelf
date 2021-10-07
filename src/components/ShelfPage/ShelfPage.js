7c2bd528238404fa74fc08b01a7384e1d01314a6
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-10-07 17:43:12 -0400

    added functionality to form. On form submit, calls handlePOSTItem to dispatch action and clear form

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 function ShelfPage() {
+  const dispatch = useDispatch();
+
   //state var to hold newItem details
-  const [newItem, setNewItem] = useState({description: '', image_url: ''});
+  const [newItem, setNewItem] = useState({ description: '', image_url: '' });
+
+  //function handePOSTItem
+  const handlePOSTItem = (event) => {
+    event.preventDefault();
+    dispatch({type: 'ADD_NEW_ITEM', payload: newItem});
+    setNewItem({ description: '', image_url: '' });
+  }
 
   return (
+    
     <div className="container">
       <h2>Shelf</h2>
       <p>All of the available items can be seen here.</p>
@@ -13,12 +24,12 @@ function ShelfPage() {
 
       {/* build 'Add' for POST route */}
       <h3>Add a new Item: </h3>
-      <form>
-        <textarea>Description</textarea>
-        <input>Image URL</input>
-        <button>submit</button>
+      <form onSubmit={handlePOSTItem}>
+        <textarea required placeholder="Item Description" value={newItem.description} type="text" onChange={(event) => setNewItem({ ...newItem, description: event.target.value })} />
+        <input required placeholder="Item Image" value={newItem.image_url} type="url" onChange={(event) => setNewItem({ ...newItem, image_url: event.target.value })} />
+        <button type="submit">submit</button>
       </form>
-    </div>
+    </div >
   );
 }
 
