a0ad34381c0ecbf5702699d4c160b17c93561b4b
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-10-07 16:04:07 -0400

    created additem form outline and newItem state var

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useState } from 'react';
 
 function ShelfPage() {
+  //state var to hold newItem details
+  const [newItem, setNewItem] = useState({description: '', image_url: ''});
+
   return (
     <div className="container">
       <h2>Shelf</h2>
@@ -8,6 +12,12 @@ function ShelfPage() {
       {/* map over items from GET */}
 
       {/* build 'Add' for POST route */}
+      <h3>Add a new Item: </h3>
+      <form>
+        <textarea>Description</textarea>
+        <input>Image URL</input>
+        <button>submit</button>
+      </form>
     </div>
   );
 }
