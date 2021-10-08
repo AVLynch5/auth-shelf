ee993cd65baef7d0cd4aba499d941088222e8388
Franck Bushbaum <franck.bushbaum@yahoo.com>
2021-10-08 13:55:48 -0500

    edit.saga.js changed from axios.delete to axios.put.

--- src/redux/sagas/edit.saga.js
+++ src/redux/sagas/edit.saga.js
@@ -5,7 +5,7 @@ function* editItem(action) {
     try {
         const theItem = action.payload
         console.log("Item to edit is: ", theItem);
-        yield axios.delete(`/api/shelf/${theItem}`);
+        yield axios.put(`/api/shelf/${theItem}`);
         yield put({ type: 'FETCH_SHELF' });
     } catch (error) {
         console.log('ERROR IN EDIT ITEM SAGA', error);
