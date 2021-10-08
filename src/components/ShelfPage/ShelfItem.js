6a2d37de7f6a0f4f46db171a80c02cff9c84a389
Greg Tambo <gregtambornino@gmail.com>
2021-10-08 11:42:37 -0500

    delete client side

--- src/components/ShelfPage/ShelfItem.js
+++ src/components/ShelfPage/ShelfItem.js
@@ -1,13 +1,21 @@
-
+import {useDispatch} from 'react-redux'
 
 function  ShelfItem ({item}) {
+    const dispatch = useDispatch()
+
+    const handleDelete = () => {
+      dispatch({ type: 'DELETE_AN_ITEM', payload: item.id })
+    }
 
     return(
         <li>Item: {item.description} 
         <br/>
         <br/>
         Photo: <img src={item.image_url} />
+        <br></br>
+        <button onClick={handleDelete}>Delete</button>
     </li>
+       
     )
 }
 
